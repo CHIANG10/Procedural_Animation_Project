9a53a66b40914847054ebd166ed4ba7a5d94c593
CHIANG10 <dobichianghk@gmail.com>
2023-08-10 12:03:29 +0900

    Tidy Up Files.

---
+++ Assets/Scripts/IK Bone/IK_Foot_Solver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IK_Foot_Solver : MonoBehaviour
+{
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Transform myTransform = this.transform;
+
+        Vector3 pos = myTransform.position;
+        pos.x += 1f;
+        pos.x -= 2f;
+
+        myTransform.position = pos;
+    }
+
+
+
+
+
+}
