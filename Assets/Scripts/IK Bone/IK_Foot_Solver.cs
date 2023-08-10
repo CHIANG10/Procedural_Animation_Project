82f0bc34cea688a076b4d232b42ef6d79efeff3e
CHIANG10 <dobichianghk@gmail.com>
2023-08-10 18:49:20 +0900

    Modified IK_Foot_Solver Script.

--- Assets/Scripts/IK Bone/IK_Foot_Solver.cs
+++ Assets/Scripts/IK Bone/IK_Foot_Solver.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class IK_Foot_Solver : MonoBehaviour
 {
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+    // todo remove this later[Range(0, 1)] [SerializeField]
+    float movementFactor; // 0 for not moved 1 for fully moved
 
-    private void Start()
+
+    Vector3 startPos;
+    // Use this for initialization
+    void Start()
     {
-
+        startPos = transform.position;
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        Transform myTransform = this.transform;
+        float cycles = Time.time / period;
 
-        Vector3 pos = myTransform.position;
-        pos.x += 1f;
-        pos.x -= 2f;
+        const float tau = Mathf.PI * 2;
+        float RawSinWave = Mathf.Sin(cycles * tau);
 
-        myTransform.position = pos;
+        movementFactor = (RawSinWave + 1f) / 2f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startPos + offset;
     }
-
-
-
-
 
 }
