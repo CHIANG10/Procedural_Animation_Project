e93318edf29b3e546fcd61b42210127ca0297a65
CHIANG10 <dobichianghk@gmail.com>
2023-08-16 18:49:17 +0900

    Rename Assets/Scripts/Control/new_bone_controller.cs -> Assets/Scripts/Control/CharacterMovement.cs

--- Assets/Scripts/Control/CharacterMovement.cs
+++ Assets/Scripts/Control/CharacterMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class new_bone_controller : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
     public Transform target;
     private NavMeshAgent myAgent;
